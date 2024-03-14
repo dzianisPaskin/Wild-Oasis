7a8e1fdb56f5d078c30dd6f1bf9b3e9ac3e32fa4
Denis <paskin.denis.00@gmail.com>
2024-03-14 16:08:34 +0100

    connected fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>App</h1>
+    </>
+  );
 };
 
 export default App;
