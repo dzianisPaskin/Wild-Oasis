26cff58a2a3691fa2a026163e7328f4b821e9a9d
Denis <paskin.denis.00@gmail.com>
2024-03-25 10:37:03 +0100

    fixed modal in a CabinRow

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -97,12 +97,12 @@ const CabinRow = ({ cabin }) => {
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
 
-          <Modal.Open>
+          <Modal.Open opens="delete">
             <button>
               <HiTrash />
             </button>
           </Modal.Open>
-          <Modal.Window>
+          <Modal.Window name="delete">
             <ConfirmDelete
               resourceName="cabin"
               onConfirm={() => deleteCabin(cabinId)}
